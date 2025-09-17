a3ae258a712b07384b33724f09176514b7f26516
Advik <advik.b@gmail.com>
2025-09-17 23:06:38 +0530

    Add files via upload

---
+++ composeApp/src/androidMain/kotlin/dev/advik/messagelogger/Platform.kt
@@ -0,0 +1,9 @@
+package dev.advik.messagelogger
+
+import android.os.Build
+
+class AndroidPlatform {
+    val name: String = "Android ${Build.VERSION.SDK_INT}"
+}
+
+fun getPlatform() = AndroidPlatform()
