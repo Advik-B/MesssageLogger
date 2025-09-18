54134ccc866885d94250dce96c09bdacd3f9a8ee
Advik <advik.b@gmail.com>
2025-09-18 06:57:49 +0530

    Update build.gradle.kts

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -43,21 +43,20 @@ kotlin {
     }
 }
 
+dependencies {
+    kapt("androidx.room:room-compiler:2.5.0")
+    debugImplementation(compose.uiTooling)
+}
+
 android {
     namespace = "dev.advik.messagelogger"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
         applicationId = "dev.advik.messagelogger"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        minSdk = libs.versions.androidMinSdk.get().toInt()
+        targetSdk = libs.versions.androidTargetSdk.get().toInt()
         versionCode = 1
         versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
     buildTypes {
         getByName("release") {
@@ -69,9 +68,3 @@ android {
         targetCompatibility = JavaVersion.VERSION_11
     }
 }
-
-dependencies {
-    debugImplementation(compose.uiTooling)
-    kapt("androidx.room:room-compiler:2.5.0")
-}
-
